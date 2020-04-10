1329a1e53ab53a518c8cbea92229549082aa1083
zdorovy90@mail.ru <zdorovy90@mail.ru>
2020-04-10 16:57:04 +0300

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/calculate/MainActivity.java
@@ -0,0 +1,83 @@
+package com.bignerdranch.android.calculate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CalculatorModel calculator;
+
+    private TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int[] numberIds = new int[] {
+                R.id.zero,
+                R.id.one,
+                R.id.two,
+                R.id.three,
+                R.id.four,
+                R.id.five,
+                R.id.six,
+                R.id.seven,
+                R.id.eight,
+                R.id.nine
+        };
+
+        int[] actionsIds = new int[] {
+                R.id.plus,
+                R.id.minus,
+                R.id.multiply,
+                R.id.division,
+                R.id.equals
+        };
+
+        text = findViewById(R.id.text);
+
+        calculator = new CalculatorModel();
+
+        final Animation animAlpha = AnimationUtils.loadAnimation(this, R.anim.set);
+
+        View.OnClickListener numberButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(animAlpha);
+                calculator.onNumPressed(view.getId());
+                text.setText(calculator.getText());
+            }
+        };
+
+        View.OnClickListener actionButtonOnclickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(animAlpha);
+                calculator.onActionPressed(view.getId());
+                text.setText(calculator.getText());
+            }
+        };
+
+        for (int i = 0; i < numberIds.length; i++) {
+            findViewById(numberIds[i]).setOnClickListener(numberButtonClickListener);
+        }
+
+        for (int i = 0; i < actionsIds.length; i++) {
+            findViewById(actionsIds[i]).setOnClickListener(actionButtonOnclickListener);
+        }
+
+        findViewById(R.id.reset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculator.reset();
+                text.setText(calculator.getText());
+            }
+        });
+    }
+}
